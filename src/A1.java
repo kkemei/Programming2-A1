3ae9ec96d898bc904c7f832752f745fc384f20e8
kkemei <kipngetich.kemei@my.jcu.edu.au>
2017-09-10 16:24:28 +1000

    implemented the bike into the grid

--- src/A1.java
+++ src/A1.java
@@ -1,33 +1,30 @@
-import java.awt.GridLayout;
+import java.awt.*;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
+
+import static javax.swing.BorderFactory.createLineBorder;
 
 public class A1 {
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Light Cycles");
 //        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500,500);
+        frame.setBackground(Color.BLUE);
+        frame.setSize(500, 500);
         frame.setResizable(false);
         frame.setLayout(new GridLayout(5, 5));
-        frame.add(new JButton("1"));
-        frame.add(new JButton("1"));
-        frame.add(new JButton("1"));
-        frame.add(new JButton("1"));
-        frame.add(new JButton("1"));
-        frame.add(new JButton("1"));
-        frame.add(new JButton("1"));
-        frame.add(new JButton("1"));
-        frame.add(new JButton("1"));
-        frame.add(new JButton("1"));
-        frame.add(new JButton("1"));
-        frame.add(new JButton("1"));
-        frame.add(new JButton("1"));
-        frame.add(new JButton("1"));
-        frame.add(new JButton("1"));
-        frame.add(new JButton("1"));
-        frame.add(new JButton("1"));
-        frame.add(new JButton("1"));
+
+        ImageIcon icon = new ImageIcon("images/bike.png");
+        icon.getImage().flush();
+        frame.add(new JLabel(icon, SwingConstants.CENTER));
+
+
+        for (int i = 0; i < 24; i++) {
+            frame.add(new JLabel(".", SwingConstants.CENTER));
+        }
+
 //        frame.pack();
         frame.setVisible(true);
     }
