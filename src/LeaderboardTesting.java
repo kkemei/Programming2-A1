8c429c33737d439927e71c44aa5a146c841834de
kkemei <kipngetich.kemei@my.jcu.edu.au>
2017-09-14 20:13:54 +1000

    testing 2

--- src/LeaderboardTesting.java
+++ src/LeaderboardTesting.java
@@ -3,6 +3,7 @@ public class LeaderboardTesting {
         makeRank(1,"Kip",1500);
         makeRank(2,"Dan",1000);
         makeRank(3,"Mike",500);
+
     }
 
     private static void makeRank(int rank, String name, int score){
