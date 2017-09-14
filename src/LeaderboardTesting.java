76a57a3376e1b2aad0aa0e43a4858e3ab81017d6
kkemei <kipngetich.kemei@my.jcu.edu.au>
2017-09-14 20:09:36 +1000

    testing 2

--- src/LeaderboardTesting.java
+++ src/LeaderboardTesting.java
@@ -1,8 +1,11 @@
 public class LeaderboardTesting {
     public static void main (String args[]){
-        int rank = 1;
-        String name = "Kip";
-        int score =1500;
-        System.out.println(rank +"||" + name + "||" + score);
+        makeRank(1,"Kip",1500);
+        makeRank(2,"Dan",1000);
+        makeRank(3,"Mike",500);
+    }
+
+    private static void makeRank(int rank, String name, int score){
+        System.out.println(rank +"     ||     " + name + "     ||     " + score);
     }
 }
