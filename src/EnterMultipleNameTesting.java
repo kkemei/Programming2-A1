235d67fce39c90a045ab6b17ddc7aa83a28eb536
kkemei <kipngetich.kemei@my.jcu.edu.au>
2017-10-19 17:33:41 +1000

    Testing multicast

--- src/EnterMultipleNameTesting.java
+++ src/EnterMultipleNameTesting.java
@@ -8,7 +8,8 @@ public class EnterMultipleNameTesting {
         numOfPlayers = JOptionPane.showInputDialog(null,
                 "How many players (no more than 22)");
         number =  Integer.parseInt(numOfPlayers);
-        for (int i = 0; i < number; i++) {
+        for (int i = 0; i < number;
+             i++) {
             names[i] = JOptionPane.showInputDialog(null,
                     "Enter players name");
             JOptionPane.showMessageDialog(null, "Hello, welcome to LightCycles " + names[i]);
